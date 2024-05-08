8b595810a1e081ab36f10266c35e4ec43635d7bb
aomarabdelaziz <abdelazizomar1337@gmail.com>
2024-05-08 11:47:16 +0300

    fa

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t maven-builder -f Dockerfile.mavenBuilder .
-docker run --rm -v "/home/abdelaziz-omar/Desktop/isfp-test/tomcat10-jakartaee9/target/":/app/target maven-builder
-docker build -t helloworld-app . 
-docker run -d -p 8080:8080 helloworld-app 
